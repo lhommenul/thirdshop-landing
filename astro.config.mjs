723b471fbd5bd365d1c5c54527e247f9560507b1
Antoine Jézéquel <antoinejezequel@interstis.fr>
2025-10-22 09:14:38 +0200

    add nodejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,35 @@
 import { defineConfig, envField } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import node from "@astrojs/node";
+
 const DEFAULT_LOCALE = "en";
 
 // https://astro.build/confi
 export default defineConfig({
-    vite: {    
-        plugins: [tailwindcss()],  
-    },
-    i18n: {
-        locales: ["en", "fr"],
-        defaultLocale: DEFAULT_LOCALE,
-        routing: {
-            prefixDefaultLocale: true,
-            fallbackType: "rewrite"
-        },
-        fallback: {
-            fr: DEFAULT_LOCALE
-        }
-    },
-    env: {
-        schema: {
-            DEFAULT_LOCALE: envField.string({ context: "client", access: "public", default: DEFAULT_LOCALE }),
-        }
-    }
+  vite: {    
+      plugins: [tailwindcss()],  
+  },
+
+  i18n: {
+      locales: ["en", "fr"],
+      defaultLocale: DEFAULT_LOCALE,
+      routing: {
+          prefixDefaultLocale: true,
+          fallbackType: "rewrite"
+      },
+      fallback: {
+          fr: DEFAULT_LOCALE
+      }
+  },
+
+  env: {
+      schema: {
+          DEFAULT_LOCALE: envField.string({ context: "client", access: "public", default: DEFAULT_LOCALE }),
+      }
+  },
+
+  adapter: node({
+    mode: "standalone"
+  })
 });
